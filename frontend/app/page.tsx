732ebcdcde1afbd355f4f29e49dbb56a3bf56e2d
Kunal-Paste <pastekunal6@gmail.com>
2026-01-09 11:42:14 +0530

    started front end

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+      <main style={{ padding: "40px" }}>
+      <h1>Echo Zone 🎧</h1>
+      <p>Welcome to my Next.js app</p>
+
+      <button className="bg-white text-black p-1.5 rounded-md font-bold">Register</button>
+    </main>
+  );
+}
